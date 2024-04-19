faf411f9536c7c42a1c476791b55a5e85f5bd5ed
Gabriel Silva Santos <gabrielss432112@gmail.com>
2024-04-19 07:13:15 -0300

    🐞 fix: saved data on localStorage

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,15 +1,62 @@
 import { Column, Task } from './models';
 
+const defaultColumns: Column[] = [
+  { id: '1', title: 'Para Fazer📝' },
+  { id: '2', title: 'Em Progresso🚀' },
+  { id: '3', title: 'Feito🚩' }
+];
+
+const defaultTasks: Task[] = [
+  {
+    id: '1',
+    columnId: '1',
+    content: 'Criar Wireframes para o Projeto 🎨'
+  },
+  {
+    id: '2',
+    columnId: '1',
+    content: 'Desenvolver Componente de Login 🔑'
+  },
+  {
+    id: '3',
+    columnId: '2',
+    content: 'Testar Funcionalidades do Sistema ⚙️'
+  },
+  {
+    id: '4',
+    columnId: '2',
+    content: 'Preparar Documentação para Entrega 📄'
+  },
+  {
+    id: '5',
+    columnId: '3',
+    content: 'Revisar Interface com o Cliente 👨‍💼'
+  }
+];
+
+function isFirstAccess(): boolean {
+  const savedColumns = localStorage.getItem('columns') ?? '[]';
+  const savedTasks = localStorage.getItem('tasks') ?? '[]';
+
+  return savedColumns === '[]' && savedTasks === '[]';
+}
+
 export function generateId(): string {
   return new Date().getTime().toString();
 }
 
 export function getSavedColumns(): Column[] {
-  const savedColumns = localStorage.getItem('columns');
-  return savedColumns ? JSON.parse(savedColumns) : [];
+  if (!isFirstAccess()) {
+    const savedColumns = localStorage.getItem('columns')!;
+    return JSON.parse(savedColumns);
+  } else {
+    localStorage.setItem('columns', JSON.stringify(defaultColumns));
+    localStorage.setItem('tasks', JSON.stringify(defaultTasks));
+    return defaultColumns;
+  }
 }
 
 export function getSavedTasks(): Task[] {
-  const savedTasks = localStorage.getItem('tasks');
+  const savedTasks = localStorage.getItem('tasks')!;
   return savedTasks ? JSON.parse(savedTasks) : [];
 }
